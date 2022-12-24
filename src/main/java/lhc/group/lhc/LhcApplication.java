2ac8b19952e4412a7f171e0fb00999d5b2f7993a
NatalieLHC <bobspange5@gmail.com>
2022-12-24 14:40:58 +0400

    commit initial create

---
+++ src/main/java/lhc/group/lhc/LhcApplication.java
@@ -0,0 +1,13 @@
+package lhc.group.lhc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LhcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lhc/group/lhc/LhcApplicationTests.java
@@ -0,0 +1,13 @@
+package lhc.group.lhc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
